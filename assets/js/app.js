86cb4c230761b4d78a0bd6e6d01b2c21fffdfe34
korede lacici <lafical@yahoo.fr>
2019-12-05 12:05:59 +0100

    Travis

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,9 +12,6 @@ require('@fortawesome/fontawesome-free/css/all.min.css');
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('@fortawesome/fontawesome-free/js/all.js');
 
-require('@fortawesome/fontawesome-free/css/all.min.css');
-require('@fortawesome/fontawesome-free/js/all.js');
-
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // eslint-disable-next-line import/no-extraneous-dependencies
 

fe3cd6b29847f017c93adce04c783cfb28b3c7cd
Florentin Wilfried <florentinwilfried@gmail.com>
2019-12-11 09:54:21 +0100

    The fonction category and tool is ok, and the loop in the offres.html.twig is ok

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -9,34 +9,40 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route ("/", name="offres")
- */
 class CategoryController extends AbstractController
 {
     /**
      * @Route ("/", name="category")
-     * @return Response A response instance
+     * @return Response
      */
 
     public function category(): Response
     {
+
         $categories = $this->getDoctrine()
             ->getRepository(Category::class)
             ->findAll();
 
-        if (!$categories) {
-            throw $this->createNotFoundException('No program found in program\'s table.');
-        }
+        return $this->render(
+            'home/index.html.twig',
+            ['categories' => $categories]
+        );
+    }
+
+    /**
+     * @Route ("/", name="tool")
+     * @return Response
+     */
+    public function tool(): Response
+    {
 
         $tools = $this->getDoctrine()
             ->getRepository(Tool::class)
-            ->findBy(['categories' => $categories]);
+            ->findAll();
 
         return $this->render(
             'home/index.html.twig',
-            ['categories' => $categories,
-            'tools' => $tools]
+            ['tools' => $tools]
         );
     }
 }

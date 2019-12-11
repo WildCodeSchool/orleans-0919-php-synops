6235652db5b960bc6a5311b09ee5a2ccda38f8c8
korede lacici <lafical@yahoo.fr>
2019-12-11 15:26:07 +0100

    list articles

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/article", name="article_")
+ */
+class ArticleController extends AbstractController
+{
+    /**
+     *  @Route("/", name="index")
+     * @return  Response
+     */
+    public function show():Response
+    {
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No Articles found in program\'s table.'
+            );
+        }
+        return $this->render('article/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+}

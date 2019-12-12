617442f0be5651755ac189c8db0dae9d367e0d93
Florentin Wilfried <florentinwilfried@gmail.com>
2019-12-12 22:54:49 +0100

    delete comment in repository tool and category, delete two migrations

--- src/Repository/ToolRepository.php
+++ src/Repository/ToolRepository.php
@@ -18,33 +18,4 @@ class ToolRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Tool::class);
     }
-
-    // /**
-    //  * @return Tool[] Returns an array of Tool objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('t.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Tool
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }

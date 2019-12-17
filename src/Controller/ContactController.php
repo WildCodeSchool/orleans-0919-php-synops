c820e7d7da3497df7784006527b49ddf9aee56f2
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2019-12-17 18:05:51 +0100

    Contact Form connected with BDD

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,20 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $contact = new Contact();
         $form = $this->createForm(ContactType::class);
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+            return $this->redirectToRoute('index');
+        }
         return $this->render('contact/index.html.twig', [
-            'our_form' => $form->createView()
+            'our_form' => $form->createView(),
         ]);
     }
 }

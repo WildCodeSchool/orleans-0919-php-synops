5e7a434582b80fe01b95b79ed1125ebcab368a17
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2019-12-22 18:43:09 +0100

    contact form is ready to use

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,21 +5,15 @@ namespace App\Controller;
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(Request $request)
+    public function index()
     {
         $form = $this->createForm(ContactType::class);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-        }
 
         return $this->render('contact/index.html.twig', [
             'our_form' => $form->createView(),

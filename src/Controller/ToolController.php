e2f398473fde0bf05249d7f0a60a330071dfbc1d
Nolwenn <sachet.nolwenn@orange.fr>
2020-01-02 00:44:35 +0100

    grump PHP

--- src/Controller/ToolController.php
+++ src/Controller/ToolController.php
@@ -3,8 +3,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use App\Repository\ToolRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class ToolController
+class ToolController extends AbstractController
 {
+    /**
+     * @Route("outils/{slug}", name="tool")
+     */
+
+    public function index(CategoryRepository $categoryRepository, Category $category, ToolRepository $toolRepository) : Response
+    {
+        $categories = $categoryRepository->find('id');
+        $tools = $toolRepository->findAll();
+
+        return $this->render('tool/index.html.twig', [
+            'category' => $category,
+            'categories' => $categories,
+            'tools' => $tools
+        ]);
+    }
 
 }

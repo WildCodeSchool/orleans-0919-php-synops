c719199444f37a6308279f5edb1c75c28dcddd01
Alexandre Coulon <alex97.coul@gmail.com>
2020-01-02 17:48:26 +0100

    S03US25_Inscription

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,7 @@ use App\Form\RegistrationFormType;
 use App\Security\LoginFormAuthentificator;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -70,7 +71,7 @@ class SecurityController extends AbstractController
                 $request,
                 $authenticator,
                 'main' // firewall name in security.yaml
-            );
+            )?: new RedirectResponse('app_index');
         }
 
         return $this->render('security/register.html.twig', [

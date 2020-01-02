5fc68171ee1455fc0840096428c53e37d169dc30
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-01-02 18:16:16 +0100

    corrections done link PDF

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,18 @@ Encore
         // pattern: /\.(png|jpg|jpeg)$/
     })
 
+    .copyFiles({
+        from: './assets/PDF',
+        // optional target path, relative to the output dir
+        to: 'PDF/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        // to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        // pattern: /\.(png|jpg|jpeg)$/
+    })
+
     /*
      * ENTRY CONFIG
      *

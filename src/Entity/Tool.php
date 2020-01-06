084833fb5499bd1d45055ef5e9702028efd6f84b
Nolwenn <sachet.nolwenn@orange.fr>
2020-01-07 00:43:48 +0100

    fixtures

--- src/Entity/Tool.php
+++ src/Entity/Tool.php
@@ -35,6 +35,11 @@ class Tool
      */
     private $category;
 
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -60,6 +65,18 @@ class Tool
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }

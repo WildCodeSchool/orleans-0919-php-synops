761da050dcccf19a1922ac3aab916db73cb87cde
Nolwenn <sachet.nolwenn@orange.fr>
2020-01-07 00:15:56 +0100

    slug category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
@@ -29,7 +30,8 @@ class Category
     private $tools;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @Gedmo\Slug(fields={"sector"})
+     * @ORM\Column(length=255, unique=true)
      */
     private $slug;
 

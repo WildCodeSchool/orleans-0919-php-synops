96cdcb8a0e76ee4e0c723fc770e802a4e71d61be
korede lacici <lafical@yahoo.fr>
2020-01-07 17:53:10 +0100

    EspaceClient

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,14 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class MemberController extends AbstractController
 {
     /**
-     * @Route("/espace-client/{id}", name="customer_space")
-     * @param User $user
+     * @Route("/espace-client", name="customer_space")
+     * @IsGranted("ROLE_USER")
      * @return  Response
      */
-    public function show(User $user): Response
+    public function show(): Response
     {
         return $this->render('user/show.html.twig', [
-            'user' => $user,
+            'user' => $this->getUser(),
         ]);
     }
 }

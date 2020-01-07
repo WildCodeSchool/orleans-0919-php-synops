001b5bb21a98723482242b0a740f619bd4b76cc4
Alexandre Coulon <alex97.coul@gmail.com>
2020-01-07 17:55:38 +0100

    Front admin members

---
+++ src/Controller/AdminUserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/user")
+ */
+class AdminUserController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_user_index", methods={"GET"})
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('admin_user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/{id}", name="admin_user_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_user_index');
+    }
+}

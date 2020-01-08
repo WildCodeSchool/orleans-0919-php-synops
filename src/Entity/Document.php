72f2a1b789199cbb33a40d110c20c17af8f9621f
Florentin Wilfried <florentinwilfried@gmail.com>
2020-01-08 11:07:44 +0100

    create entity Document relation manyToOne with entity Tool

---
+++ src/Entity/Document.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DocumentRepository")
+ */
+class Document
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Tool", inversedBy="documents")
+     */
+    private $tool;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTool(): ?self
+    {
+        return $this->tool;
+    }
+
+    public function setTool(?self $tool): self
+    {
+        $this->tool = $tool;
+
+        return $this;
+    }
+}
--- src/Entity/Tool.php
+++ src/Entity/Tool.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
@@ -40,6 +42,16 @@ class Tool
      */
     private $description;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Document", mappedBy="tool")
+     */
+    private $documents;
+
+    public function __construct()
+    {
+        $this->documents = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -77,6 +89,37 @@ class Tool
     public function setDescription(?string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Document[]
+     */
+    public function getDocuments(): Collection
+    {
+        return $this->documents;
+    }
+
+    public function addDocument(Document $document): self
+    {
+        if (!$this->documents->contains($document)) {
+            $this->documents[] = $document;
+            $document->setTool($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDocument(Document $document): self
+    {
+        if ($this->documents->contains($document)) {
+            $this->documents->removeElement($document);
+            // set the owning side to null (unless already changed)
+            if ($document->getTool() === $this) {
+                $document->setTool(null);
+            }
+        }
 
         return $this;
     }

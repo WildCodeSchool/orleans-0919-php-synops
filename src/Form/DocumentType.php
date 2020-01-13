14ae724b0b7eda4060fb8a239ff12f27f8fd21a9
Florentin Wilfried <florentinwilfried@gmail.com>
2020-01-13 15:51:12 +0100

    corrections

--- src/Form/DocumentType.php
+++ src/Form/DocumentType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Document;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,9 @@ class DocumentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('description')
-            ->add('tool', null, ['choice_label' => 'name'])
+            ->add('description', TextareaType::class, ['label' => "Description de l'outil"])
+            ->add('tool', null, ['choice_label' => 'name',
+                'label' => 'Le nom des sous-catégories'])
         ;
     }
 
--- src/Form/ToolType.php
+++ src/Form/ToolType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Tool;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,8 @@ class ToolType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('category', null, ['choice_label' => 'sector'])
-            ->add('name');
+            ->add('category', null, ['choice_label' => 'sector', 'label' => 'Le nom des catégories'])
+            ->add('name', TextType::class, ['label' => "Le nom de la catégorie"]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

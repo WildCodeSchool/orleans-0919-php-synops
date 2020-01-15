cacbe01815fb16111efa86b5a2dd56a954c9a5b2
korede lacici <lafical@yahoo.fr>
2020-01-15 10:15:15 +0100

    Creation nouveau membre equipe

--- src/Form/TeamType.php
+++ src/Form/TeamType.php
@@ -3,20 +3,34 @@
 namespace App\Form;
 
 use App\Entity\Team;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class TeamType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
-            ->add('description')
-            ->add('picture')
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'LinkedIn'
+            ])
+            ->add('description', CKEditorType::class, [
+                'label' => 'Description'
+            ])
+            ->add('picture', FileType::class, [
+                'label' => 'Image'
+            ])
         ;
     }
 

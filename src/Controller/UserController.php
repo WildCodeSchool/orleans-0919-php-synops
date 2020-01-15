404a405883fd18c945702f65ef4162c588da89eb
Nolwenn <sachet.nolwenn@orange.fr>
2020-01-15 14:44:52 +0100

    mise en page du formulaire

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -16,39 +16,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/", name="user_index", methods={"GET"})
-     */
-    public function index(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="user_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('user_index');
-        }
-
-        return $this->render('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
      * @Route("/{id}", name="user_show", methods={"GET"})
      */
     public function show(User $user): Response
@@ -76,19 +43,5 @@ class UserController extends AbstractController
             'user' => $user,
             'form' => $form->createView(),
         ]);
-    }
-
-    /**
-     * @Route("/{id}", name="user_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, User $user): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($user);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('user_index');
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\User;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,17 +14,27 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('company')
-            ->add('phone')
-            ->add('function')
-            ->add('field')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('token')
-        ;
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'Adresse E-mail'
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Numéro de téléphone'
+            ])
+            ->add('company', TextType::class, [
+                'label' => 'Nom de l\'entreprise'
+            ])
+            ->add('field', TextType::class, [
+                'label' => 'Secteur d\'activité de l\'entreprise'
+            ])
+            ->add('function', TextType::class, [
+                'label' => 'Fonction au sein de l\'entreprise'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

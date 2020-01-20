3da1cae2239bd0e10db80e6f0feb8ebadb1e4264
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-01-20 15:08:48 +0100

    list article by section

---
+++ src/Form/ArticleSearchType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Section;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleSearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->setMethod('GET')
+            ->add('search', SearchType::class, [
+                'required' => false
+            ])
+            ->add('section', EntityType::class, [
+                'class' => Section::class,
+                'required' => false,
+                'choice_label' => 'name',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => false,
+        ]);
+    }
+}

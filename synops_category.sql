66547ec8dc050efc0223e6a2393941e6b1f5a698
Nolwenn <sachet.nolwenn@orange.fr>
2020-02-03 10:57:08 +0100

    id enlevés

--- synops_category.sql
+++ synops_category.sql
@@ -4,8 +4,8 @@ LIMIT 0, 1000
 
 -- Date: 2020-02-03 09:21
 */
-INSERT INTO `category` (`id`,`sector`,`slug`) VALUES (1,'Accompagnement RH','accompagnement-rh');
-INSERT INTO `category` (`id`,`sector`,`slug`) VALUES (2,'Appui commercial','appui-commercial');
-INSERT INTO `category` (`id`,`sector`,`slug`) VALUES (3,'Appui marketing & communication','appui-marketing-communication');
-INSERT INTO `category` (`id`,`sector`,`slug`) VALUES (4,'Conduite et gestion de projets','conduite-et-gestion-de-projets');
-INSERT INTO `category` (`id`,`sector`,`slug`) VALUES (5,'Formation','formation');
+INSERT INTO `category` (`sector`,`slug`) VALUES ('Accompagnement RH','accompagnement-rh');
+INSERT INTO `category` (`sector`,`slug`) VALUES ('Appui commercial','appui-commercial');
+INSERT INTO `category` (`sector`,`slug`) VALUES ('Appui marketing & communication','appui-marketing-communication');
+INSERT INTO `category` (`sector`,`slug`) VALUES ('Conduite et gestion de projets','conduite-et-gestion-de-projets');
+INSERT INTO `category` (`sector`,`slug`) VALUES ('Formation','formation');
